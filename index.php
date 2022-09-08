08eebba1fb8df1ff596755f5aa25f11077314230
RodrigoBaltazarDuran <kodeb.robadues@gmail.com>
2022-09-07 21:28:50 -0500

    update view principal

---
+++ index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap versión 5 CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
+    <!-- Fontawesome cdn -->
+    <script src="https://kit.fontawesome.com/7e55e8c0f3.js" crossorigin="anonymous"></script>
+    <title>Grupo Axis</title>
+</head>
+<body">
+
+    <!-- Barra de navegación -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand text-center" href="#">
+                <p class="m-0">GRUPO</p>
+                <p class="m-0">AXIS</p>
+            </a>
+            <!-- <a class="navbar-brand" href="#">
+                <img src="/assets/image/logo.png" height="70vh" alt="logo">
+            </a> -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            PRODUCTOS
+                        </a>
+                        <ul class="dropdown-menu bg-secondary">
+                            <li class="border-bottom">
+                                <a class="dropdown-item text-white" href="#">Calderas</a>
+                            </li>
+                            <li class="border-bottom">
+                                <a class="dropdown-item text-white" href="#">Recuperadores de Calor</a>
+                            </li>
+                            <!-- <li class="border-bottom"><hr class="dropdown-divider"></li> -->
+                            <li class="border-bottom">
+                                <a class="dropdown-item text-white" href="#">Calentadores</a>
+                            </li>
+                            <li class="border-bottom">
+                                <a class="dropdown-item text-white" href="#">Recipientes</a>
+                            </li>
+                            <li class="border-bottom">
+                                <a class="dropdown-item text-white" href="#">Columnas</a>
+                            </li>
+                            <li class="border-bottom">
+                                <a class="dropdown-item text-white" href="#">Chimeneas</a>
+                            </li>
+                            <li class="border-bottom">
+                                <a class="dropdown-item text-white" href="#">Condensadores</a>
+                            </li>
+                            <li class="border-bottom">
+                                <a class="dropdown-item text-white" href="#">Intercambiadores</a>
+                            </li>
+                            <li class="border-bottom">
+                                <a class="dropdown-item text-white" href="#">Quemadores</a>
+                            </li>
+                            <li class="border-bottom">
+                                <a class="dropdown-item text-white" href="#">Tapas</a>
+                            </li>
+                            <li class="border-bottom">
+                                <a class="dropdown-item text-white" href="#">Formas Metalicas</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item text-white" href="#">Otros Servicios</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            GRUPO AXIS
+                        </a>
+                        <ul class="dropdown-menu bg-secondary">
+                            <li class="border-bottom">
+                                <a class="dropdown-item text-white" href="#">Foster Wheeler Mexicana</a>
+                            </li>
+                            <li class="border-bottom">
+                                <a class="dropdown-item text-white" href="#">Energía Peabody</a>
+                            </li>
+                            <!-- <li class="border-bottom"><hr class="dropdown-divider"></li> -->
+                            <li>
+                                <a class="dropdown-item text-white" href="#">Video Institucional</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            DESCARGAS
+                        </a>
+                        <ul class="dropdown-menu bg-secondary">
+                            <li class="border-bottom">
+                                <a class="dropdown-item text-white" href="#">Foster Wheeler Mexicana</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item text-white" href="#">Energía Peabody</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">NOTICIAS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">CONTACTO </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Carrusel de imagenes -->
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="/assets/image/imagePrincipal3.jpg" class="d-block w-100" alt="imagePrincipal">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>GRUPO AXIS</h5>
+                    <p>45 AÑOS DE EXCELENCIA INDUSTRIAL</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="/assets/image/imagePrincipal2.jpg" class="d-block w-100" alt="imagePrincipal2">
+                <!-- <div class="carousel-caption d-none d-md-block">
+                    <h5>Second slide label</h5>
+                    <p>Some representative placeholder content for the second slide.</p>
+                </div> -->
+            </div>
+            <div class="carousel-item">
+                <img src="/assets/image/imagePrincipal.jpg" class="d-block w-100" alt="imagePrincipal3">
+                <!-- <div class="carousel-caption d-none d-md-block">
+                    <h5>Third slide label</h5>
+                    <p>Some representative placeholder content for the third slide.</p>
+                </div> -->
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+
+    <div style="width: 90%; margin: auto;">
+        <!-- DATO IMPORTANTE -->
+        <h2 class="text-center py-3 fw-bolder border-bottom border-secondary" style="color: #EB540A;">
+            45 AÑOS DE EXCELENCIA INDUSTRIAL    
+        </h2>
+        <!-- OBJETIVOS -->
+        <div class="d-flex justify-content-around py-3 border-bottom border-secondary">
+            <div class="me-3">
+                <h3 style="color: #EB540A; font-size: 1.3rem;">OBJETIVO</h3>
+                <div class="d-flex">
+                    <p class="me-3">icono</p>
+                    <p style="font-size: 0.8em;">Diseñar y fabricar equipos para la producción de refinados y petroquímicos, dando absoluta prioridad al medio ambiente.</p>
+                </div>
+            </div>
+            <div class="me-3">
+                <h3 style="color: #EB540A; font-size: 1.3rem;">VISIÓN</h3>
+                <div class="d-flex">
+                    <p class="me-3">icono</p>
+                    <p style="font-size: 0.8em;">Posicionar a GRUPO AXIS como el corporativo líder en el mercado nacional e internacional de bienes de capital.</p>
+                </div>
+            </div>
+            <div class="me-3">
+                <h3 style="color: #EB540A; font-size: 1.3rem;">ESPÍRITU AXIS</h3>
+                <div class="d-flex">
+                    <p class="me-3">icono</p>
+                    <p style="font-size: 0.8em;">Cumplimiento en tiempo y presupuesto, trabajo en equipo, responsabilidad social, enfoque a mejora continua.</p>
+                </div>
+            </div>
+        </div>
+        <!-- INTERESES -->
+        <div class="d-flex justify-content-around py-3 border-bottom border-secondary">
+            <div class="me-3">
+                <h3 style="color: #EB540A; font-size: 1.3rem;">OBJETIVO</h3>
+                <div class="d-flex">
+                    <p class="me-3">icono</p>
+                    <p style="font-size: 0.8em;">Diseñar y fabricar equipos para la producción de refinados y petroquímicos, dando absoluta prioridad al medio ambiente.</p>
+                </div>
+            </div>
+            <div class="me-3">
+                <h3 style="color: #EB540A; font-size: 1.3rem;">VISIÓN</h3>
+                <div class="d-flex">
+                    <p class="me-3">icono</p>
+                    <p style="font-size: 0.8em;">Posicionar a GRUPO AXIS como el corporativo líder en el mercado nacional e internacional de bienes de capital.</p>
+                </div>
+            </div>
+            <div class="me-3">
+                <h3 style="color: #EB540A; font-size: 1.3rem;">ESPÍRITU AXIS</h3>
+                <div class="d-flex">
+                    <p class="me-3">icono</p>
+                    <p style="font-size: 0.8em;">Cumplimiento en tiempo y presupuesto, trabajo en equipo, responsabilidad social, enfoque a mejora continua.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- <img src="/assets/image/imagePrincipal.jpg" class="img-fluid" alt="imagePrincipal"> -->
+    <!-- Bootstrap versión 5 JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
